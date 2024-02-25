78c675bbcb7e686181e9fc02e49a34b1f189871d
abdulkadirhavran <ahavranlioglu@hotmail.com>
2024-02-25 13:08:51 +0300

    changes

--- Js-5214/script.js
+++ Js-5214/script.js
@@ -1,6 +1,7 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
-  if (x.className === "topnav") {
+  if (x.className === "topnav") 
+  {
     x.classList.add("responsive");
   } else {
     x.className = "topnav";
