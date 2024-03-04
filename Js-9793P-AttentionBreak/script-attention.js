025df127aaf1a5fb91d5e667413b57d54dcbfd12
abdulkadirhavran <ahavranlioglu@hotmail.com>
2024-03-04 08:42:32 +0300

    updates

--- Js-9793P-AttentionBreak/script-attention.js
+++ Js-9793P-AttentionBreak/script-attention.js
@@ -2,14 +2,16 @@ let value = 0;
 
 function increase() {
   document.getElementById("increaseNumber").innerHTML = ++value;
-  if (value == 10) {
+  if (value === 11) {
     alert("Fazla telefona baktın odağını kaybetmemelisin");
-  } else if (value == 20) {
+  } else if (value === 21) {
     alert("Biraz müzik iyi gelir");
     /* 20'ye gelince otomatik music çalması için şart bloğu yaptım */
-    if(value == 20){
+    if(value == 21){
       classicMusic.play();
     }
+  }else if (value == 31){
+    prompt("Dikkatinizi neden toplayamıyorsunuz? Açıklarmısınız :");
   }
 }
 
