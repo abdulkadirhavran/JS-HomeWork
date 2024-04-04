4a0c56abe8bb091c616cc895005c58d85e7236b8
Abdulkadir Havranlıoğlu <153865169+abdulkadirhavran@users.noreply.github.com>
2024-04-04 21:06:18 +0300

    changes

---
+++ NasaAPI/dist/script_nasa.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function fetchNeoData() {
+  var apikey;
+  return regeneratorRuntime.async(function fetchNeoData$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          apikey = "ZizZ80kFNINa6gkw6GoKXHjGtcB1DvCPpk1Nb9wp";
+
+        case 1:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
---
+++ NasaAPI/script_nasa.js
@@ -0,0 +1,6 @@
+async function fetchNeoData() {
+    const apikey = "ZizZ80kFNINa6gkw6GoKXHjGtcB1DvCPpk1Nb9wp";
+
+
+
+}
